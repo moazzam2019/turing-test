ac71aa537fded0cdfd90bd3c9f453ce51d3f2792
moazzam2019 <moazzam.iqbal2019@gmail.com>
2023-06-24 19:27:10 +0500

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/header/header";
+import Login from "./routes/login/login";
+import Home from "./routes/home/home";
+import { PrivateRoute } from "./Routing/Routing";
+import axios from "axios";
+import { useEffect } from "react";
 
 const App = () => {
   document.title = "Turing Test";
+  useEffect(() => {
+    // Function to send the request and get a new access token
+    const fetchNewAccessToken = async () => {
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_BASE_URL}/auth/refresh-token`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+            },
+          }
+        );
+
+        const newAccessToken = response.data.access_token;
+        // Store the new access token in local storage
+        localStorage.setItem("access_token", newAccessToken);
+      } catch (error) {
+        // Handle error
+        console.error("Error refreshing access token:", error);
+      }
+    };
+
+    // Call the function immediately
+    fetchNewAccessToken();
+
+    // Set an interval to call the function every 10 minutes
+    const intervalId = setInterval(fetchNewAccessToken, 10 * 60 * 1000);
+
+    // Clean up the interval on component unmount
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
   return (
-    <Routes>
-      {/* <Route path="/" element={<HeaderFooter />}>
-        <Route index element={<Home />} />
-        <Route path="about-us" element={<About />} />
-        <Route path="contact-us" element={<Contact />} />
-      </Route> */}
-    </Routes>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <PrivateRoute path="/home" exact component={Home} />
+      </Switch>
+    </Router>
   );
 };
 
