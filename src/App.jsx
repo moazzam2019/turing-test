f57ad3ccba68bdeb2bacbd3909eabcfb8de7a8da
moazzam2019 <moazzam.iqbal2019@gmail.com>
2023-06-24 10:14:04 +0500

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  document.title = "Turing Test";
+  return (
+    <Routes>
+      {/* <Route path="/" element={<HeaderFooter />}>
+        <Route index element={<Home />} />
+        <Route path="about-us" element={<About />} />
+        <Route path="contact-us" element={<Contact />} />
+      </Route> */}
+    </Routes>
+  );
+};
+
+export default App;
